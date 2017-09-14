4400c15da4f8f7e717bcbdf5cbf7493c84424040
Xavier Mendez <me@jmendeth.com>
2017-09-14 22:38:54 +0200

    Add uncommited changes & push PDFs, repo now dead

--- buildall.sh
+++ buildall.sh
@@ -7,5 +7,5 @@ cp pgflibrary*.tex tikzlibrary*.tex build
 # Build things
 cd build
 for l in ../T?-*.tex; do
-  pdflatex $l < /dev/null || exit 1
+  pdflatex -shell-escape $l < /dev/null || exit 1
 done
